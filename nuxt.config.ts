1d9e445efbf7d4e6708a2dd3324b69c2c1d3dc0d
Jan Ackermann <eric-ackermann@hotmail.de>
2022-10-10 14:37:05 +0200

    Fix build issues

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@ import { defineNuxtConfig } from 'nuxt/config'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-    rootDir: "./src",
+    srcDir: "src/",
     modules: [
         '@nuxtjs/tailwindcss'
     ],

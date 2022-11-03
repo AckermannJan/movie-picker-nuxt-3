bb4e8f1d2f7a8fe214b0a9fee18b7fb2654e9b36
Jan Ackermann <jan.ackermann@real-digital.de>
2022-11-03 19:13:14 +0100

    Push latest state

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,19 +2,18 @@ import { defineNuxtConfig } from 'nuxt/config'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-    srcDir: "src/",
-    modules: [
-        '@nuxtjs/tailwindcss',
-        [
-            '@pinia/nuxt', {
-                autoImports: [
-                    'defineStore'
-                ]
-            },
-        ],
+  srcDir: 'src/',
+  modules: [
+    '@nuxtjs/tailwindcss',
+    [
+      '@pinia/nuxt',
+      {
+        autoImports: ['defineStore'],
+      },
     ],
-    runtimeConfig: {
-        notionSecret: process.env.NOTION_SECRET,
-        tmdbSecret: process.env.TMDB_SECRET,
-    },
+  ],
+  runtimeConfig: {
+    notionSecret: process.env.NOTION_SECRET,
+    tmdbSecret: process.env.TMDB_SECRET,
+  },
 })
